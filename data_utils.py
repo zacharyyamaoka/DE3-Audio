21948912ea982d235529a4079a0594d9361327d4
Zachary Yamaoka <zach.yamaoka@gmail.com>
2019-03-11 09:37:07 +0000

    Fixing Ind correspondance

--- data_utils.py
+++ data_utils.py
@@ -13,7 +13,7 @@ def load_data_file(n=1,file_stem="data_rec",sample_rate = 44100,label_rate = 1, 
     zeros = "00"
     chunk = 1024
     label = np.loadtxt("data_label/"+file_stem+zeros+str(n)+".txt")
-
+    num_labels = label.shape[0]
     #vars for vizualize
     room_width=5
     room_length=5
@@ -26,27 +26,23 @@ def load_data_file(n=1,file_stem="data_rec",sample_rate = 44100,label_rate = 1, 
 
     raw_data = player.get_all_data()
 
-    #regardless clip of first 2 seconds if data
+    #regardless clip of first 2 seconds if data #b/c small not meaningful start at the begging
     start_clip = sample_rate * 2 #seconds
     data = raw_data[start_clip:-1]
     num_sample = data.shape[0]
 
     # find start and end
     start = 0
-    end = num_sample - 1
-
     for i in np.arange(num_sample):
         if data[i,0] != 0:
             start = i
             break;
-
-    for i in np.arange(num_sample):
-        j = num_sample - 1 - i
-        if data[j,0] != 0:
-            end = j
-            break;
-
-    data = data[start:end] #remove silence
+    print("num labels, ", num_labels)
+    label_time = num_labels * label_rate
+    num_sample_labeled = label_time * sample_rate
+    end = start + num_sample_labeled
+    data = data[start:end,:]
+    #find end based on num of samples
     if playback: #for debugging, then play back data to make sure it is valid
         print("Playback Audio")
         parsed_audio = encode(data)
@@ -78,7 +74,10 @@ def load_data_file(n=1,file_stem="data_rec",sample_rate = 44100,label_rate = 1, 
 def sample2labelId(n, sample_rate,label_rate): #get label which correlates with sample
     timestamp = n/sample_rate
     print(timestamp)
-    label_num = int(round(timestamp*label_rate)) - 1 #so you can index correctly in list
+
+    #Used to be -1 but that does not give the correct behavour
+    #With audio samples you want to floor(). Any samples should correspond to the last label, not the nearest... do not round()
+    label_num = int(timestamp*label_rate) #so you can index correctly in list
     return label_num
 
 
@@ -92,4 +91,8 @@ print("Data Params")
 print("Audio Stero Vector: ", data[0].shape, " Sample Rate: ", data[2][0])
 print("Position Label: ", data[1].shape, " Label Rate: ", data[2][1])
 
-ind = sample2labelId(2703871, 44100, 1) #will be some small error, but no more then 1 label so its ok....
+ind = sample2labelId(0, 44100, 1) #will be some small error, but no more then 1 label so its ok....
+# assert ind == 0
+print(ind)
+ind = sample2labelId(data[0].shape[0], 44100, 1) #will be some small error, but no more then 1 label so its ok....
+print(ind)
